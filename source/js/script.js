fe6daaf131e53152b696ac57737d8656a5c10c25
konstantin arzamastsev <volgavolga1995@yandex.ru>
2023-06-15 04:02:49 +0300

    стилизация мобилки

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.main-header');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-header--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-header--closed')) {
+    navMain.classList.remove('main-header--closed');
+    navMain.classList.add('main-header--opened');
+  } else {
+    navMain.classList.add('main-header--closed');
+    navMain.classList.remove('main-header--opened');
+  }
+});
